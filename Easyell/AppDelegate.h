3fb9b8cbc4672a21b327b7fb8b4391d8284305a2
guoshencheng <648772021@qq.com>
2015-03-09 10:28:00 +0800

    first commit

---
+++ Easyell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Easyell
+//
+//  Created by guoshencheng on 3/9/15.
+//  Copyright (c) 2015 guoshencheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
