b4b6f3d8494b247927588dad2a634524748de0f5
guoshencheng <648772021@qq.com>
2015-03-11 13:01:45 +0800

    Save color changes

---
+++ Easyell/ViewControllers/HomeViewController.m
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.m
+//  Easyell
+//
+//  Created by guoshencheng on 3/10/15.
+//  Copyright (c) 2015 guoshencheng. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
